65ca6ea0886e0c1e8f54c269279b188e19e0dddb
Bouraoui abdallah <ingbouraouiabdallah@gmail.com>
2024-04-22 17:31:40 +0100

    ajout des componsants lié a la dashboard

--- pizza-time/src/app/dashboard/Components/DashHeader.tsx
+++ pizza-time/src/app/dashboard/Components/DashHeader.tsx
@@ -34,7 +34,7 @@ const DashHeader = ({ userId }: Props) => {
             <header className="grid grid-cols-2 py-5 px-4 md:px-12 items-center sticky top-0 z-10 rounded-md bg-white shadow-md">
                 {/* Left Area */}
                 <div className="flex items-center justify-end gap-x-8">
-                    <Image src="/logo.png" width={40} height={40} alt="logo" />
+                    <Image src="/Objects/logo.jpg" width={100} height={100} alt="logo" />
                 </div>
                 {/* Right Area */}
                 <div className="flex items-center  justify-end gap-x-4">
---
+++ pizza-time/src/app/dashboard/deliveries/page.tsx
@@ -0,0 +1,18 @@
+
+
+import Container from '@/app/common/Container'
+import DeliveriesTable from './DeliveriesTable'
+
+
+const AdminDeliveries = () => {
+  return (
+    <Container>
+
+      <DeliveriesTable/>
+    </Container>
+
+   
+  )
+}
+
+export default AdminDeliveries
