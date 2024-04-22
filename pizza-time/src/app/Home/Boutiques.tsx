5259fbf04f59fccfef36239da391df5574d81f14
Bouraoui abdallah <ingbouraouiabdallah@gmail.com>
2024-04-22 09:56:56 +0100

    affichage du map

--- pizza-time/src/app/Home/Boutiques.tsx
+++ pizza-time/src/app/Home/Boutiques.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { card } from "../../constats";
+import Map from '../Maps/page';
 const Boutiques = () => {
 
   const getCurrentTime = () => {
@@ -10,38 +11,39 @@ const Boutiques = () => {
     return `${hours}:${minutes}`;
   };
 
-  const isBoutiqueOpen = (openingTime:any, closingTime:any) => {
+  const isBoutiqueOpen = (openingTime: any, closingTime: any) => {
     const currentTime = getCurrentTime();
     return currentTime >= openingTime && currentTime <= closingTime;
   };
 
   return (
-    <div  className='text-3xl font-semibold text-center text-gray-600 sm:text-4xl'>Nos Boutiques
+    <div className='text-3xl font-semibold text-center text-gray-600 sm:text-4xl'>Nos Boutiques
       <div className="container text-center">
         <div className="row">
-        {Object.values(card.shoplist).map((shoplist:any,index) => {
-          const isOpen = isBoutiqueOpen(shoplist.openingTime, shoplist.closingTime);
-          return (
-            <div className="col" key={index}>
-            <Link href={`/boutiques/${shoplist.shopid}`} key={shoplist.id}>
-              <div className={`bg-white rounded-lg shadow-md overflow-hidden cursor-pointer ${isOpen ? 'border-green-500' : 'border-red-500'}`}>
-                <img src={shoplist.image} alt={shoplist.title} className="w-full h-48 object-cover" />
-                <div className="p-4">
-                  <h3 className="text-lg font-semibold">{shoplist.Address}</h3>
-                  <p className="text-gray-600">Company: {shoplist.Company}</p>
-                  <p className="text-gray-600">Country: {shoplist.Country}</p>
-                  <p className="text-gray-600">PostalCode: {shoplist.PostalCode}</p>
-                  <p className={`text-${isOpen ? 'green-600' : 'red-600'}`}>
-                    {isOpen ? 'Open' : 'Closed'}
-                  </p>
-                </div>
+          {Object.values(card.shoplist).map((shoplist: any, index) => {
+            const isOpen = isBoutiqueOpen(shoplist.openingTime, shoplist.closingTime);
+            return (
+              <div className="col" key={index}>
+                <Link href={`/boutiques/${shoplist.shopid}`} key={shoplist.id}>
+                  <div className={`bg-white rounded-lg shadow-md overflow-hidden cursor-pointer ${isOpen ? 'border-green-500' : 'border-red-500'}`}>
+                    <img src={shoplist.image} alt={shoplist.title} className="w-full h-48 object-cover" />
+                    <div className="p-4">
+                      <h3 className="text-lg font-semibold">{shoplist.Address}</h3>
+                      <p className="text-gray-600">Company: {shoplist.Company}</p>
+                      <p className="text-gray-600">Country: {shoplist.Country}</p>
+                      <p className="text-gray-600">PostalCode: {shoplist.PostalCode}</p>
+                      <p className={`text-${isOpen ? 'green-600' : 'red-600'}`}>
+                        {isOpen ? 'Open' : 'Closed'}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
       </div>
+      <Map />
     </div>
   );
 }
