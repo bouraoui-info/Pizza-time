50ac2b68889a227d68739dc2fe987afe71b25ee9
Bouraoui abdallah <ingbouraouiabdallah@gmail.com>
2024-05-10 15:03:34 +0100

    fixation

--- pizza-time/src/app/dashboard/menu/AdminMenuTable.tsx
+++ pizza-time/src/app/dashboard/menu/AdminMenuTable.tsx
@@ -107,7 +107,7 @@ const handleClick=(idresto:number)=>{
                                     className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
                                     onClick={() => handleDelete(item.resto.id)}
                                 >
-                                    Supprimer  Categories
+                                    Supprimer  Restaurant
                                 </button>
                             </td>
                             <td className="px-6 py-3">
